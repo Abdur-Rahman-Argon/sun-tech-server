3b53fc978533a9d63000497783da1dbcb7b19885
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2022-12-12 15:39:32 +0600

    sun-tech express server ready

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const { product } = require("./Products");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(" server is Running");
+});
+
+app.get("/product", (req, res) => {
+  res.send(product);
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
